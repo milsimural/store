0ed9f3a9c689bf6ff74c1c30d4cb617bf5a8fffe
Mitshan <milsimural@gmail.com>
2023-11-23 16:53:39 +0300

    Make object

---
+++ domain/Store/Proj.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Store
+{
+    public class Proj
+    {
+        public int Id { get; }
+
+        public string Title { get; }
+
+        public Proj(int id, string title)
+        {
+            Id = id;
+            Title = title;
+        }
+    }
+}
