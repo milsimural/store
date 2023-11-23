b3682a5cde1a4c951a00b5eefb35a39b3c23e2bb
Mitshan <milsimural@gmail.com>
2023-11-23 17:46:46 +0300

    Make Repos

---
+++ infrastructure/Store.Memory/ProjRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Store.Memory
+{
+    public class ProjRepository : IProjRepository
+    {
+        private readonly Proj[] projs = new[]
+        {
+            new Proj(1, "Art Of Programing"),
+            new Proj(2, "Refactoring"),
+            new Proj(3, "C Programming Language"),
+        };
+
+        public Proj[] GetAllByTitle(string titlePart)
+        {
+            return projs.Where(proj => proj.Title.Contains(titlePart))
+                .ToArray();
+        }
+    }
+}
